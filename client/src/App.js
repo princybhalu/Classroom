ffb76398cdc3b3be5443c9564d12d46880f4fdcf
princybhalu <princybhalu11@gmail.com>
2023-01-20 12:18:14 +0530

    Simaple Rounting For login and registertion

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,27 @@
 import Registration  from './auth/registration/registration';
 import { toast, ToastContainer } from 'react-toastify';
+import { BrowserRouter as Router, Routes, Route, Navigate, Link } from 'react-router-dom';
+import Login from './auth/login/login';
+import  Dashboard from './dashboard';
 
 function App() {
   return (
-    <div className="App">
-      <Registration />
-      <ToastContainer position={toast.POSITION.TOP_RIGHT} autoClose={200000} />
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+        
+          <Route exact path='/' element={ < Dashboard />} ></Route>
+          <Route path='/auth'>
+            <Route exact path='/auth/login' element={ < Login />}></Route>
+            <Route exact path='/auth/register' element={< Registration />}></Route>
+          </Route>
+
+        </Routes>
+
+
+        <ToastContainer position={toast.POSITION.TOP_RIGHT} autoClose={200000} />
+      </div>
+    </Router>
   );
 }
 
